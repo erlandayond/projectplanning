e721a7dad36cbaefa25e9ece6cb9ef98a87ae80e
suresh siddagari <suresh.csea5@gmail.com>
2012-12-13 11:11:52 +0100

    adding quarter values

--- app/models/EmployeeInfo.java
+++ app/models/EmployeeInfo.java
@@ -1,5 +1,6 @@
 package models;
 import java.util.ArrayList;
+import java.util.Dictionary;
 import java.util.List;
 
 public class EmployeeInfo {
@@ -8,6 +9,7 @@ public class EmployeeInfo {
 		public String strEmpName;
 		public String strEmpType;
 		public List<ProjectOccupied> listProjectWorking= new ArrayList<ProjectOccupied>();
-		
+		public Dictionary dicProjectWorking;
+		public List<Integer> listQuarterOccupied =new ArrayList<Integer>(13);  // 13 is quarter size
 		
 }
